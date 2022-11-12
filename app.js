24598bfa34be6c72c8c1f25e5f37a13afea0a5e5
Mikka2020 <takahashi.yuji.as01@gmail.com>
2022-11-12 12:11:55 +0900

    routingとmysql接続を追加

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public", { index: false }));
+app.use(express.static(__dirname + "/views", { index: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// DB接続
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  port: 3306,
+  database: "auction_db",
+});
+connection.connect((error) => {
+  if (error) {
+    console.log("error connecting:" + error.stack);
+    return;
+  }
+  console.log("success");
+});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+app.get("/auctions", (req, res) => {
+  res.render("auctions.ejs");
+});
+app.get("/auctions/:auctionId", (req, res) => {
+  res.render("auction.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/mypage", (req, res) => {
+  res.render("mypage.ejs");
+});
+
+app.listen(9000);
