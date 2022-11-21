db9389eb0ce7cdadf04d0561c290240db42cb13c
OhtaniKoki <93703473+OhtaniKoki@users.noreply.github.com>
2022-11-21 14:27:33 +0900

    作業中です

--- test.js
+++ test.js
@@ -1,1 +1,33 @@
-kdkdkdkdkdkdkdkkddkdkdkdkdkkd
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'auction_db'
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('success');
+  });
+
+  app.get('/', (req, res) => {
+    connection.query(
+      'SELECT * FROM car',
+      (error, results) => {
+        // console.log(results);
+        res.render('hello.ejs',{data:results[0]});
+      }
+    );
+  });
+
+
+  
+  app.listen(9000);
