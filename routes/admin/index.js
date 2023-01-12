007c70744d67f403559e7e87d83d7a617631fb7f
Mikka2020 <takahashi.yuji.as01@gmail.com>
2023-01-12 16:40:42 +0900

    (WIP)管理の車両登録作成途中

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,10 +3,12 @@ const mysql = require("mysql");
 const db = require("../../database.js");
 const connection = mysql.createConnection(db);
 
+// 管理者トップ
 router.get('/', (req, res) => {
   res.render('admin/index');
 });
 
+// イベント一覧
 router.get('/events', (req, res) => {
   const sql = `
     SELECT
@@ -25,6 +27,7 @@ router.get('/events', (req, res) => {
   );
 });
 
+// イベント登録
 router.post('/events', (req, res) => {
   const sql = `
     INSERT INTO
@@ -40,7 +43,7 @@ router.post('/events', (req, res) => {
   );
 });
 
-
+// 車両一覧
 router.get('/cars', (req, res) => {
   const sql = `
     SELECT
@@ -72,11 +75,26 @@ router.get('/cars', (req, res) => {
   );
 });
 
+// 車両登録
 router.get('/cars/register/', (req, res) => {
-  res.render('admin/carRegister');
+  // メーカー一覧
+  const sql = `
+    SELECT
+      id,
+      manufacture_name
+    FROM
+      manufacturer
+  `;
+  connection.query(
+    sql,
+    (error, results) => {
+      console.log(results);
+      res.render('admin/carRegister', { manufacturerList: results });
+    }
+  );
 });
-
-router.get('/cars/:id', (req, res) => {
+// 車両詳細
+router.get('/cars/:id/', (req, res) => {
   const sql = `
     SELECT
       e.start_time,
@@ -133,12 +151,83 @@ router.get('/cars/:id', (req, res) => {
     });
     return eventDateList;
   }
-
   Promise.all([getCarItem(), getEventDateList()]).then((results) => {
     res.render('admin/carItem', { data: results[0], eventDateList: results[1] });
   });
-
 });
 
+// 車両出品
+router.get('/cars/:id/register', (req, res) => {
+  const sql = `
+    SELECT
+      e.start_time,
+      e.end_time,
+      c.car_model_name,
+      b.bodytype_name,
+      c.id AS car_id,
+      c.mileage_situation,
+      c.number_passengers,
+      c.repair_history,
+      c.car_inspection_expiration_date,
+      c.mileage,
+      e.lowest_winning_bid
+    FROM
+      exhibit AS e
+    LEFT JOIN car AS c
+    ON
+      e.car_id = c.id
+    LEFT JOIN bodytype AS b
+    ON
+      c.body_type_id = b.id
+      `;
+  async function getCarItem() {
+    const carItem = await new Promise((resolve, reject) => {
+      connection.query(
+        sql,
+        [req.params.id],
+        (error, results) => {
+          resolve(results[0]);
+        }
+      );
+    });
+    return carItem;
+  }
+  async function getEventDateList() {
+    const eventDateList = await new Promise((resolve, reject) => {
+      const sql = `
+        SELECT
+          eventdate.id,
+          eventdate.event_date
+        FROM
+          eventdate
+        WHERE
+          event_date >= CURDATE()
+        ORDER BY
+          eventdate.event_date
+        DESC`;
+      connection.query(
+        sql,
+        (error, results) => {
+          resolve(results);
+        }
+      );
+    });
+    return eventDateList;
+  }
+  Promise.all([getCarItem(), getEventDateList()]).then((results) => {
+    res.render('admin/carExhibit', { data: results[0], eventDateList: results[1] });
+  });
+});
+
+// 車両出品
+router.post('/cars/:id/register', (req, res) => {
+  const sql = ``
+  connection.query(
+    sql,
+    (error, results) => {
+      res.redirect('/admin/cars');
+    }
+  );
+});
 
 module.exports = router;
