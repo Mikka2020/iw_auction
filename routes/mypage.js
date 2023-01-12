2f30ac6aeff010a9fc76c985638d41f58f885c2e
Mikka2020 <takahashi.yuji.as01@gmail.com>
2023-01-12 20:04:13 +0900

    通知画面できた

--- routes/mypage.js
+++ routes/mypage.js
@@ -46,4 +46,37 @@ router.get("/orders", (req, res) => {
       res.render("orders", { orders: result });
     })
 });
+
+router.get("/notifications", (req, res) => {
+  const isAuth = req.isAuthenticated();
+  const sql = `
+    SELECT
+      successful_bid.id AS successful_bid_id,
+      successful_bid.created_at AS successful_bid_created_at,
+      car.id AS car_id,
+      manufacturer.manufacture_name AS manufacturer_name,
+      car.model_year,
+      car.grade,
+      car.model,
+      successful_bid.successful_bid_price,
+      successful_bid.payment_status
+    FROM
+    successful_bid
+    JOIN exhibit ON successful_bid.exhibit_id = exhibit.id
+    JOIN car ON car.id = exhibit.car_id
+    JOIN manufacturer ON manufacturer.id = car.manufacturer_id
+    WHERE
+      successful_bid.user_id = ?
+    ORDER BY
+      successful_bid.created_at
+    DESC;
+    `;
+  connection
+    .query(sql, [req.session.passport.user.id], (err, result) => {
+      if (err) throw err;
+      const user = req.session.passport ? req.session.passport.user : null;
+      console.log(result);
+      res.render("notification", { notifications: result, isAuth: isAuth, user: user });
+    });
+});
 module.exports = router;
